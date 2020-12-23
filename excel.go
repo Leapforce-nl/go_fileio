fb91dbe9ec8432eb7bc252315929e38472846477
caselongo <caselongo@gmail.com>
2020-12-23 14:25:31 +0100

    x

--- excel.go
+++ excel.go
@@ -1,8 +1,14 @@
 package fileio
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
+	"strconv"
+	"strings"
+	"time"
+
+	"cloud.google.com/go/civil"
 
 	errortools "github.com/leapforce-libraries/go_errortools"
 	excel "github.com/szyhf/go-excel"
@@ -50,4 +56,32 @@ func readExcel(conn excel.Connecter, config *excel.Config, model interface{}) *e
 	}
 
 	return nil
+}
+
+func ParseExcelDate(value string) (*civil.Date, *errortools.Error) {
+	value = strings.Trim(value, " ")
+
+	if value == "" {
+		return nil, nil
+	}
+
+	t, err := time.Parse("02-01-2006", value)
+	if err != nil {
+		t, err = time.Parse("2-1-2006", value)
+	}
+	if err == nil {
+		date := civil.DateOf(t)
+		return &date, nil
+	}
+
+	dateInt, err := strconv.Atoi(value)
+
+	if err == nil {
+		// Excel "day 0" is 1899-12-30
+		t0, _ := time.Parse("2006-01-02", "1899-12-30")
+		date := civil.DateOf(t0.Add(time.Duration(dateInt*24) * time.Hour))
+		return &date, nil
+	}
+
+	return nil, errortools.ErrorMessage(fmt.Sprintf("Cannot convert '%s' to civil.Date", value))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/leapforce-libraries/go_fileio
 go 1.13
 
 require (
+	cloud.google.com/go v0.74.0
 	github.com/leapforce-libraries/go_errortools v0.0.0-20201204210737-d975f1003e2b
 	github.com/leapforce-libraries/go_utilities v0.0.0-20201219095534-b51e50abef10
 	github.com/szyhf/go-excel v1.5.1
